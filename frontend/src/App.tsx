f03e0b46cc634ed911db0c2ba7f7ff58ec2fda3b
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-23 17:38:51 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+      suscipit deserunt porro repellat fugiat placeat provident corrupti quod
+      amet ipsum voluptates itaque maxime officiis, libero maiores laboriosam
+      laborum recusandae. Corporis.
+    </div>
+  );
+};
+
+export default App;
