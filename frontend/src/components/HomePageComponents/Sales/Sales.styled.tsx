841c69a1df46a6379e750e1722f8b1cda7d507ae
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-29 19:26:45 +0200

    Sales block in Home page

---
+++ frontend/src/components/HomePageComponents/Sales/Sales.styled.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const SalesStyled = styled.section`
+  display: block;
+  padding: 80px 17.9%;
+  margin-bottom: 100px;
+`;
+
+export const SalesStyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SalesStyledButton = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 80px;
+`;
+
+export const SalesStyledList = styled.div`
+  display: flex;
+`;
