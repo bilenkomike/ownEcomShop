7ae8d995e1a462d3146aa5f1aae6450cf920584a
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-01 14:42:13 +0200

    added ability to toggle megamenu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
+import React, { useState } from "react";
 import { HeaderStyled } from "./Header.styled";
-import React from "react";
 import MainMenu from "../MainMenu/MainMenu";
 import TopBar from "../TopBar/TopBar";
 import SpecialOffers from "../SpecialOffers/SpecialOffers";
 import MegaMenu from "../MegaMenu/MegaMenu";
 
-const Header: React.FC = () => {
+const Header: React.FC<{ toggleBackDrop: () => void }> = ({
+  toggleBackDrop,
+}) => {
+  const [megamenu, setMegamenu] = useState(false);
+  const toggleMegaMenu = () => {
+    setMegamenu(!megamenu);
+    toggleBackDrop();
+  };
   return (
     <HeaderStyled>
       <TopBar />
-      <MainMenu />
-      <MegaMenu />
+      <MainMenu toggle={toggleMegaMenu} />
+      <MegaMenu active={megamenu} />
       <SpecialOffers />
     </HeaderStyled>
   );
