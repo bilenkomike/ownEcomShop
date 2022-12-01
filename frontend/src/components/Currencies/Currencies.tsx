039b87bba531b9ad71588ffaaf09c190cd552b8e
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-01 15:38:08 +0200

    added currencies list component

---
+++ frontend/src/components/Currencies/Currencies.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { CurrenciesStyled, CurrenciesStyledItem } from "./Currencies.styled";
+
+const Currencies: React.FC<{ active: boolean }> = ({ active }) => {
+  return (
+    <CurrenciesStyled active={active}>
+      <CurrenciesStyledItem>$ USD</CurrenciesStyledItem>
+      <CurrenciesStyledItem>€ EUR</CurrenciesStyledItem>
+      <CurrenciesStyledItem>₴ UAH</CurrenciesStyledItem>
+    </CurrenciesStyled>
+  );
+};
+
+export default Currencies;
