94a065a87f3759983886d20f7c152ca92b2f9f69
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-01 14:48:01 +0200

    added carousel to sales component

--- frontend/src/components/HomePageComponents/Sales/Sales.tsx
+++ frontend/src/components/HomePageComponents/Sales/Sales.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../../Title/Title";
 import Product from "../../Product/Product";
 import SliedControls from "../../SlideControls/SliedControls";
@@ -9,45 +9,116 @@ import {
   SalesStyledButton,
   SalesStyledHeader,
   SalesStyledList,
+  SalesStyledContainer,
 } from "./Sales.styled";
+import Carousel from "../../Carousel/Carousel";
 
 import img1 from "./images/image_1.png";
 import img2 from "./images/image_2.png";
 import img3 from "./images/image_3.png";
 
 const Sales = () => {
+  const [page, setPage] = useState(0);
+
+  const actionBack = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const actionForward = () => {
+    if (page < 2) {
+      setPage(page + 1);
+    }
+  };
   return (
     <SalesStyled>
       <SalesStyledHeader>
         <Title>Sale up to 70%</Title>
-        <SliedControls />
+        <SliedControls actionBack={actionBack} actionForward={actionForward} />
       </SalesStyledHeader>
-      <SalesStyledList>
-        <Product
-          size="large"
-          id="some"
-          name="Shirt with insertion lace trims"
-          gallary={[img1]}
-          price={49.95}
-          currency="$"
-        />
-        <Product
-          size="large"
-          id="some"
-          name="Chrono watch with blue leather belt"
-          gallary={[img2]}
-          price={49.95}
-          currency="$"
-        />
-        <Product
-          size="large"
-          id="some"
-          name="Check coat with colour contrast"
-          gallary={[img3]}
-          price={49.95}
-          currency="$"
-        />
-      </SalesStyledList>
+      <SalesStyledContainer>
+        <Carousel widthType="%" currentSlide={page}>
+          <SalesStyledList>
+            <Product
+              size="large"
+              id="some"
+              name="Shirt with insertion lace trims"
+              gallary={[img1]}
+              price={49.95}
+              currency="$"
+            />
+            <Product
+              size="large"
+              id="some"
+              name="Chrono watch with blue leather belt"
+              gallary={[img2]}
+              price={49.95}
+              currency="$"
+            />
+            <Product
+              size="large"
+              id="some"
+              name="Check coat with colour contrast"
+              gallary={[img3]}
+              price={49.95}
+              currency="$"
+            />
+          </SalesStyledList>
+          <SalesStyledList>
+            <Product
+              size="large"
+              id="some"
+              name="Shirt with insertion lace trims"
+              gallary={[img1]}
+              price={49.95}
+              currency="$"
+            />
+            <Product
+              size="large"
+              id="some"
+              name="Chrono watch with blue leather belt"
+              gallary={[img2]}
+              price={49.95}
+              currency="$"
+            />
+            <Product
+              size="large"
+              id="some"
+              name="Check coat with colour contrast"
+              gallary={[img3]}
+              price={49.95}
+              currency="$"
+            />
+          </SalesStyledList>
+          <SalesStyledList>
+            <Product
+              size="large"
+              id="some"
+              name="Shirt with insertion lace trims"
+              gallary={[img1]}
+              price={49.95}
+              currency="$"
+            />
+            <Product
+              size="large"
+              id="some"
+              name="Chrono watch with blue leather belt"
+              gallary={[img2]}
+              price={49.95}
+              currency="$"
+            />
+            <Product
+              size="large"
+              id="some"
+              name="Check coat with colour contrast"
+              gallary={[img3]}
+              price={49.95}
+              currency="$"
+            />
+          </SalesStyledList>
+        </Carousel>
+      </SalesStyledContainer>
 
       <SalesStyledButton>
         <Button variant="outlined" size="large" styleType="default">
