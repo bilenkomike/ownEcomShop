44cdba355cbf80cbb374d9bb5b33f0ec266d04be
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-01 15:38:41 +0200

    removed useless useState hook

--- frontend/src/components/Currencies/Currencies.tsx
+++ frontend/src/components/Currencies/Currencies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { CurrenciesStyled, CurrenciesStyledItem } from "./Currencies.styled";
 
 const Currencies: React.FC<{ active: boolean }> = ({ active }) => {
