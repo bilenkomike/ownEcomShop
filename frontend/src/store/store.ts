a692f427f88bae790c52ff44fa77892de9f4a332
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-03 12:46:45 +0200

    added first slice to cart

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-export const store = configureStore({
-  reducer: {},
+import CartSlice from "./CartSlice/index";
+
+const store = configureStore({
+  reducer: {
+    [CartSlice.name]: CartSlice.reducer,
+  },
 });
+
+console.log(store);
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
