c3712483de5ce707a91ff2d92e3efdc33802903d
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-03 12:46:22 +0200

    added mini component

---
+++ frontend/src/components/MiniCart/MiniCart.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { CartActions } from "../../store/CartSlice";
+import { useTypedDispatch, useTypedSelector } from "../../store/hooks";
+
+import {
+  MiniCartStyled,
+  MiniCartStyledContent,
+  MiniCartStyledHeader,
+  MiniCartStyledInnerContent,
+  MiniCartStyledList,
+  MiniCartStyledTotal,
+  MiniCartStyledTotalText,
+  MiniCartStyledInnerColumn,
+  MiniCartStyledListInner,
+} from "./MiniCart.styled";
+
+import { CgClose } from "react-icons/cg";
+
+import MiniCartItem from "./MiniCartItem";
+import Button from "../Button/Button";
+
+const MiniCart: React.FC = () => {
+  const miniCartActive = useTypedSelector((state) => state.cart.MiniCartActive);
+  console.log(miniCartActive);
+
+  const dispatch = useTypedDispatch();
+
+  return (
+    <MiniCartStyled
+      active={miniCartActive}
+      onClick={() => dispatch(CartActions.miniCartActiveToggle())}
+    >
+      <MiniCartStyledInnerContent>
+        <MiniCartStyledContent
+          active={miniCartActive}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <MiniCartStyledInnerColumn>
+            <MiniCartStyledHeader>
+              Your cart (4)
+              <CgClose
+                onClick={() => dispatch(CartActions.miniCartActiveToggle())}
+              />
+            </MiniCartStyledHeader>
+            <MiniCartStyledListInner>
+              <MiniCartStyledList>
+                <MiniCartItem />
+                <MiniCartItem />
+                <MiniCartItem />
+                <MiniCartItem />
+              </MiniCartStyledList>
+            </MiniCartStyledListInner>
+
+            <MiniCartStyledTotal>
+              <MiniCartStyledTotalText>
+                Subtotal:
+                <span>$198.65</span>
+              </MiniCartStyledTotalText>
+              <Button
+                icon="card"
+                iconPosition="before"
+                styleType="default"
+                variant="solid"
+                size="large"
+                type="button"
+              >
+                Checkout
+              </Button>
+            </MiniCartStyledTotal>
+          </MiniCartStyledInnerColumn>
+        </MiniCartStyledContent>
+      </MiniCartStyledInnerContent>
+    </MiniCartStyled>
+  );
+};
+
+export default MiniCart;
---
+++ frontend/src/components/MiniCart/MiniCartItem.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+// import { CartActions } from "../../store/CartSlice";
+// import { useTypedDispatch, useTypedSelector } from "../../store/hooks";
+import img from "./images/image.png";
+
+import { BiTrash } from "react-icons/bi";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+
+import {
+  MiniCartStyledListItem,
+  MiniCartStyledListItemImage,
+  MiniCartStyledListItemContent,
+  MiniCartStyledListItemTitle,
+  MiniCartStyledListItemAttributes,
+  MiniCartStyledListItemAttributesItem,
+  MiniCartStyledListPriceBlock,
+  MiniCartStyledPrice,
+  MiniCartStyledPriceSaleFrom,
+  MiniCartStyledPriceText,
+  MiniCartStyledListItemRemoveButton,
+  MiniCartStyledListItemWishList,
+} from "./MiniCart.styled";
+import Input from "../Input/Input";
+
+const MiniCartItem: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <MiniCartStyledListItem>
+      <MiniCartStyledListItemImage
+        src={img}
+        alt="Basic hooded sweatshirt in pink"
+      />
+      <MiniCartStyledListItemContent>
+        <MiniCartStyledListItemTitle>
+          Basic hooded sweatshirt in pink
+        </MiniCartStyledListItemTitle>
+        <MiniCartStyledListItemAttributes>
+          <MiniCartStyledListItemAttributesItem>
+            Color: <span>pink </span>
+          </MiniCartStyledListItemAttributesItem>
+          <MiniCartStyledListItemAttributesItem>
+            Size: <span>S </span>
+          </MiniCartStyledListItemAttributesItem>
+        </MiniCartStyledListItemAttributes>
+        <MiniCartStyledListPriceBlock>
+          <Input
+            variant="small"
+            type="number"
+            value={counter}
+            onChange={(number) => setCounter(Number(number))}
+          />
+          <MiniCartStyledPrice>
+            <MiniCartStyledPriceText sale={true}>
+              {/* sale={sale > 0} */}
+              $15.50
+              {/* {sale === 0 && `${currency} ${price}`} */}
+              {/* {sale > 0 &&
+                `${currency} ${(price - (price * sale) / 100).toFixed(2)}`} */}
+            </MiniCartStyledPriceText>
+
+            <MiniCartStyledPriceSaleFrom>
+              $31.00
+              {/* {currency}
+                {price} */}
+            </MiniCartStyledPriceSaleFrom>
+            {/* {sale > 0 && (
+              
+            )} */}
+          </MiniCartStyledPrice>
+        </MiniCartStyledListPriceBlock>
+
+        <MiniCartStyledListItemWishList active>
+          Move to
+          {true && <AiFillHeart />}
+          {false && <AiOutlineHeart />}
+        </MiniCartStyledListItemWishList>
+      </MiniCartStyledListItemContent>
+      <MiniCartStyledListItemRemoveButton>
+        <BiTrash />
+      </MiniCartStyledListItemRemoveButton>
+    </MiniCartStyledListItem>
+  );
+};
+
+export default MiniCartItem;
