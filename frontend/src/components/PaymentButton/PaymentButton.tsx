9484b75e181e8c43fe0b1cfd84bee7d39f858303
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-14 15:36:34 +0200

    Swapped to icons with type svg for better view and search abilities

--- frontend/src/components/PaymentButton/PaymentButton.tsx
+++ frontend/src/components/PaymentButton/PaymentButton.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { PaymentButtonStyled } from "./PaymentButton.styled";
-import paypal from "./images/paypal.png";
-import visa from "./images/visa.png";
-import mastercard from "./images/mastercard.png";
+import paypal from "./images/paypal.svg";
+import visa from "./images/visa.svg";
+import mastercard from "./images/mastercard.svg";
 
 const PaymentButton: React.FC<{ type: "visa" | "mastercard" | "paypal" }> = ({
   type,
 }) => {
   return (
-    <PaymentButtonStyled>
+    <PaymentButtonStyled type="button">
       {type === "paypal" && <img src={paypal} alt="paypal" />}
       {type === "visa" && <img src={visa} alt="visa" />}
-      {type === "mastercard" && <img src={mastercard} alt="mastercard" />}
+      {type === "mastercard" && (
+        <>
+          <img src={mastercard} alt="mastercard" style={{ width: "40%" }} />
+          MasterCard
+        </>
+      )}
     </PaymentButtonStyled>
   );
 };
