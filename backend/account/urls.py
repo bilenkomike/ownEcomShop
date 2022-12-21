29c0e43bd3e31a874b159c3f64098002671708dc
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-12-21 15:31:00 +0200

    created all user views and seralizers

---
+++ backend/account/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+# from .views import user_views as views
+
+from .views import RetrieveUserById,MyTokenObtainPairView,UsersViewSet,ProfileViewSet,UserAPIView,RegisterUserApiView,DeleteUser,UpdateUser
+
+
+urlpatterns = [
+    path('users/', UsersViewSet.as_view(), name="users"),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', RegisterUserApiView.as_view(), name="register"),
+    path('profile/', ProfileViewSet.as_view(), name="profile"),
+    path('profile/update/', UserAPIView.as_view(), name="profile-update"),
+    
+
+    path('<str:pk>/', RetrieveUserById.as_view(),name='user'),
+
+    path('update/<str:pk>/', UpdateUser.as_view(),name='user-update'),
+
+    path('delete/<str:pk>/', DeleteUser.as_view(),name='user-delete'),
+]
+
+
---
+++ backend/account/views.py
@@ -0,0 +1,141 @@
+from django.contrib.auth.models import User
+
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser,AllowAny
+
+
+from .serializers import UserSerializer, UserSerializerWithToken,MyTokenObtainPairSerializer
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework import generics
+
+
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
+
+
+
+class RegisterUserApiView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializerWithToken
+    permission_classes = (AllowAny,)
+    
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        print(request.data)
+        
+        user = User.objects.create(
+            first_name=data['fullname'],
+            email=data['email'],
+            username=data['email'],
+            password = make_password(data['password'])
+        )
+        
+        user.save()
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+
+
+
+
+class UserAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    
+    def update(self, request, *args, **kwargs):
+        
+        
+        
+        user = request.user
+        
+        for i in request.data:
+            if i == 'fullname':
+                user.first_name = request.data[i]
+            if i == 'password':
+                user.password = make_password(request.data['password'])
+            if i == 'email':
+                user['username'] = request.data[i]
+                user['email'] = request.data[i]
+        
+        serializer = UserSerializer(user, many=False)
+        user.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+class ProfileViewSet(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+
+
+
+
+
+class UsersViewSet(generics.ListAPIView):
+    model = User
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+    
+
+
+
+class DeleteUser(generics.RetrieveDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
+    
+    
+
+
+class RetrieveUserById(generics.RetrieveAPIView):
+    model = User
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    lookup_field = 'pk'
+    permission_classes = [IsAdminUser]
+    
+    
+    
+    
+
+
+class UpdateUser(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+    
+    def get_queryset(self, pk):
+        
+        return User.objects.get(id=pk)
+    
+    
+    
+    def update(self, request,pk):
+        
+        
+        user = self.get_queryset(pk)
+        for i in request.data:
+            if i == 'fullname':
+                user.first_name = request.data[i]
+            if i == 'email':
+                user['username'] = request.data[i]
+                user['email'] = request.data[i]
+                
+        
+        
+        serializer = UserSerializer(user, many=False)
+        user.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
